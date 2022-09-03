237b498523fc7fdc3dca215f7226bb8dbd79145b
NikitaT206 <acid3602@yandex.ru>
2022-09-03 04:28:38 +0300

    try fix limit

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json())
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
