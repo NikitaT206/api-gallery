0adb0d1f45aaf985d79035b15c853773cf68cf99
NikitaT206 <acid3602@yandex.ru>
2022-09-03 04:21:37 +0300

    fix: file upload limit

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const {PORT, MONGO_URL} = process.env
 
 const app = express();
 app.use('/uploads', express.static('uploads'))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
